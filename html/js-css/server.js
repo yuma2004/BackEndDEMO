92fbc052207df7506328b66f5832477c16479cec
tanishi1i <yuuyakun0302@gmail.com>
2024-10-30 00:03:19 +0900

    finishHTML

---
+++ html/js-css/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const app = express();
+const mongoose = require("mongoose");
+
+app.listen(3000, () =>{
+    console.log("サーバーが起動しました")
+})
